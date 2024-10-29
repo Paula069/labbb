0af1c9eb497b2097ed20f8e96463e4397af2f521
Paula069 <paula-mora@upc.edu.co>
2024-10-28 23:38:41 -0500

    Update ServicioCatalogoMock.java

--- soporte-clases-master/mueblesdelosalpes.backend/src/main/java/co/edu/uniandes/csw/mueblesdelosalpes/logica/ejb/ServicioCatalogoMock.java
+++ soporte-clases-master/mueblesdelosalpes.backend/src/main/java/co/edu/uniandes/csw/mueblesdelosalpes/logica/ejb/ServicioCatalogoMock.java
@@ -1,14 +1,4 @@
-/**
- * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
- * $Id$ ServicioCatalogoMock.java
- * Universidad de los Andes (Bogotá - Colombia)
- * Departamento de Ingeniería de Sistemas y Computación
- * Licenciado bajo el esquema Academic Free License version 3.0
- *
- * Ejercicio: Muebles de los Alpes
- * Autor: Juan Sebastián Urrego
- * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
- */
+
 
 package co.edu.uniandes.csw.mueblesdelosalpes.logica.ejb;
 
@@ -25,45 +15,20 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.ejb.Stateless;
 
-/**
- * Implementacion de los servicios del catálogo de muebles que se le prestan al sistema.
- * @author Juan Sebastián Urrego
- */
+
 @Stateless
 public class ServicioCatalogoMock implements IServicioCatalogoMockRemote,IServicioCatalogoMockLocal
 {
 
-    //-----------------------------------------------------------
-    // Atributos
-    //-----------------------------------------------------------
-
-    /**
-     * Interface con referencia al servicio de persistencia en el sistema
-     */
     private IServicioPersistenciaMockLocal persistencia;
 
-    //-----------------------------------------------------------
-    // Constructor
-    //-----------------------------------------------------------
-
-    /**
-     * Constructor sin argumentos de la clase
-     */
     public ServicioCatalogoMock()
     {
         persistencia=new ServicioPersistenciaMock();
-        //Inicializa el arreglo de los muebles
+      
   
     }
 
-    //-----------------------------------------------------------
-    // Métodos
-    //-----------------------------------------------------------
-
-    /**
-     * Agrega un mueble al sistema
-     * @param mueble Nuevo mueble
-     */
  
     public void agregarMueble(Mueble mueble)
     {
@@ -78,10 +43,7 @@ public class ServicioCatalogoMock implements IServicioCatalogoMockRemote,IServic
        
     }
 
-    /**
-     * Se elimina un mueble del sistema dado su identificador único
-     * @param id Identificador único del mueble
-     */
+    
     @Override
     public void eliminarMueble(long id)
     {
@@ -96,10 +58,6 @@ public class ServicioCatalogoMock implements IServicioCatalogoMockRemote,IServic
         }
     }
 
-    /**
-     * Remueve un ejemplar del mueble (no el mueble)
-     * @param id Identificador único del mueble
-     */
     @Override
     public void removerEjemplarMueble(long id)
     {
@@ -118,11 +76,7 @@ public class ServicioCatalogoMock implements IServicioCatalogoMockRemote,IServic
         }
     }
 
-    /**
-     * Devuelve los muebles del sistema
-     * @return muebles Arreglo con todos los muebles del sistema
-     */
- 
+    
     public List<Mueble> darMuebles()
     {
         return persistencia.findAll(Mueble.class);
