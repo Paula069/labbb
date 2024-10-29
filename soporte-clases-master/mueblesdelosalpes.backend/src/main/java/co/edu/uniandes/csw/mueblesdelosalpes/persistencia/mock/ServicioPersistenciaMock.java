8ffc887e7d729d250ee18aad97362c8a3cd0a8c3
Paula069 <paula-mora@upc.edu.co>
2024-10-28 23:47:40 -0500

    Update ServicioPersistenciaMock.java

--- soporte-clases-master/mueblesdelosalpes.backend/src/main/java/co/edu/uniandes/csw/mueblesdelosalpes/persistencia/mock/ServicioPersistenciaMock.java
+++ soporte-clases-master/mueblesdelosalpes.backend/src/main/java/co/edu/uniandes/csw/mueblesdelosalpes/persistencia/mock/ServicioPersistenciaMock.java
@@ -1,14 +1,3 @@
-/**
- * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
- * $Id$ ServicioPersistenciaMock.java
- * Universidad de los Andes (Bogotá - Colombia)
- * Departamento de Ingeniería de Sistemas y Computación
- * Licenciado bajo el esquema Academic Free License version 3.0
- *
- * Ejercicio: Muebles de los Alpes
- * Autor: Juan Sebastián Urrego
- * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
- */
 
 package co.edu.uniandes.csw.mueblesdelosalpes.persistencia.mock;
 
@@ -30,44 +19,15 @@ import java.util.List;
 import java.util.Random;
 import javax.ejb.Stateless;
 
-/**
- * Implementación de los servicios de persistencia
- * @author Juan Sebastián Urrego
- */
+
 
 public class ServicioPersistenciaMock implements IServicioPersistenciaMockRemote, IServicioPersistenciaMockLocal {
 
-    //-----------------------------------------------------------
-    // Atributos
-    //-----------------------------------------------------------
-
-    /**
-     * Lista con los vendedores del sistema
-     */
+  
     private static ArrayList<Vendedor> vendedores;
-
-    /**
-     * Lista con los muebles del sistema
-     */
     private static ArrayList<Mueble> muebles;
-
-    /**
-     * Lista con los usuarios del sistema
-     */
     private static ArrayList<Usuario> usuarios;
-
-    /**
-     * Lista con los registros de ventas
-     */
     private static ArrayList<RegistroVenta> registrosVentas;
-
-    //-----------------------------------------------------------
-    // Constructor
-    //-----------------------------------------------------------
-
-    /**
-     * Constructor de la clase. Inicializa los atributos.
-     */
     public ServicioPersistenciaMock()
     {
         if (vendedores == null)
@@ -102,10 +62,9 @@ public class ServicioPersistenciaMock implements IServicioPersistenciaMockRemote
             muebles.add(new Mueble(7L, "Silla Neoclásica", "Una bella silla con un estilo neoclásico", TipoMueble.Exterior, 65, "neoClasica", 678));
             muebles.add(new Mueble(8L, "Camarote junior", "Con diseño moderno. Sus hijos ahora podrán tener unos felices sueños.", TipoMueble.Interior, 85, "camarote", 56565));
 
-            //Inicializa el arreglo que contiene los usuarios
+        
             usuarios = new ArrayList<Usuario>();
 
-            //Agrega usuarios al sistema
             usuarios.add(new Usuario("admin", "adminadmin", TipoUsuario.Administrador));
             usuarios.add(new Usuario("client", "clientclient", TipoUsuario.Cliente));
 
@@ -121,14 +80,6 @@ public class ServicioPersistenciaMock implements IServicioPersistenciaMockRemote
         }
     }
 
-    //-----------------------------------------------------------
-    // Métodos
-    //-----------------------------------------------------------
-    
-    /**
-     * Permite crear un objeto dentro de la persistencia del sistema.
-     * @param obj Objeto que representa la instancia de la entidad que se quiere crear.
-     */
     @Override
     public void create(Object obj) throws OperacionInvalidaException
     {
@@ -167,10 +118,6 @@ public class ServicioPersistenciaMock implements IServicioPersistenciaMockRemote
         }
     }
 
-    /**
-     * Permite modificar un objeto dentro de la persistencia del sistema.
-     * @param obj Objeto que representa la instancia de la entidad que se quiere modificar.
-     */
     @Override
     public void update(Object obj)
     {
@@ -221,10 +168,6 @@ public class ServicioPersistenciaMock implements IServicioPersistenciaMockRemote
         }
     }
 
-    /**
-     * Permite borrar un objeto dentro de la persistencia del sistema.
-     * @param obj Objeto que representa la instancia de la entidad que se quiere borrar.
-     */
     @Override
     public void delete(Object obj) throws OperacionInvalidaException
     {
@@ -286,11 +229,7 @@ public class ServicioPersistenciaMock implements IServicioPersistenciaMockRemote
         }
     }
 
-    /**
-     * Retorna la lista de todos los elementos de una clase dada que se encuentran en el sistema.
-     * @param c Clase cuyos objetos quieren encontrarse en el sistema.
-     * @return list Listado de todos los objetos de una clase dada que se encuentran en el sistema.
-     */
+  
     @Override
     public List findAll(Class c)
     {
@@ -316,12 +255,7 @@ public class ServicioPersistenciaMock implements IServicioPersistenciaMockRemote
         }
     }
 
-    /**
-     * Retorna la instancia de una entidad dado un identificador y la clase de la entidadi.
-     * @param c Clase de la instancia que se quiere buscar.
-     * @param id Identificador unico del objeto.
-     * @return obj Resultado de la consulta.
-     */
+  
     @Override
     public Object findById(Class c, Object id)
     {
