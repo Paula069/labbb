54f7259e20b9a7085244407ce1af92e86a985118
Paula069 <paula-mora@upc.edu.co>
2024-10-28 23:46:34 -0500

    Update IServicioVendedoresMockRemote.java

--- soporte-clases-master/mueblesdelosalpes.backend/src/main/java/co/edu/uniandes/csw/mueblesdelosalpes/logica/interfaces/IServicioVendedoresMockRemote.java
+++ soporte-clases-master/mueblesdelosalpes.backend/src/main/java/co/edu/uniandes/csw/mueblesdelosalpes/logica/interfaces/IServicioVendedoresMockRemote.java
@@ -1,14 +1,4 @@
-/**
- * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
- * $Id$ IServicioVendedoresMockRemote.java
- * Universidad de los Andes (Bogotá - Colombia)
- * Departamento de Ingeniería de Sistemas y Computación
- * Licenciado bajo el esquema Academic Free License version 3.0
- *
- * Ejercicio: Muebles de los Alpes
- * Autor: Juan Sebastián Urrego
- * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
- */
+
 
 package co.edu.uniandes.csw.mueblesdelosalpes.logica.interfaces;
 
@@ -18,30 +8,11 @@ import co.edu.uniandes.csw.mueblesdelosalpes.excepciones.OperacionInvalidaExcept
 import java.util.List;
 import javax.ejb.Remote;
 
-/**
- * Contrato funcional de los servicios de administración de los vendedores del sistema
- * @author Juan Sebastián Urrego
- */
 @Remote
 public interface IServicioVendedoresMockRemote
 {
-    /**
-     * Agrega un vendedor al sistema
-     * @param vendedor Nuevo vendedor
-     * @throws OperacionInvalidaException Excepción lanzada en caso de error operacional
-     */
+    
     public void agregarVendedor(Vendedor vendedor)throws OperacionInvalidaException;
-
-    /**
-     * Elimina un vendedor del sistema
-     * @param id Número de identificación único del cliente
-     * @throws OperacionInvalidaException Excepción lanzada en caso de error operacional
-     */
     public void eliminarVendedor(long id)throws OperacionInvalidaException;
-
-    /**
-     * Devuelve todos los vendedores del sistema
-     * @return vendedores Vendedores del sistema
-     */
     public List<Vendedor> getVendedores();
 }
