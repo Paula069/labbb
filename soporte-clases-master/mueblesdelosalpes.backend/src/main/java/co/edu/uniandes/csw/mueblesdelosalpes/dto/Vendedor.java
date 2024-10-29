b3ee2363d6ee27f16029bd36b453bbe48d647ece
Paula069 <paula-mora@upc.edu.co>
2024-10-28 23:36:26 -0500

    Update Vendedor.java

--- soporte-clases-master/mueblesdelosalpes.backend/src/main/java/co/edu/uniandes/csw/mueblesdelosalpes/dto/Vendedor.java
+++ soporte-clases-master/mueblesdelosalpes.backend/src/main/java/co/edu/uniandes/csw/mueblesdelosalpes/dto/Vendedor.java
@@ -1,93 +1,33 @@
-/**
- * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
- * $Id$ Vendedor.java
- * Universidad de los Andes (Bogotá - Colombia)
- * Departamento de Ingeniería de Sistemas y Computación
- * Licenciado bajo el esquema Academic Free License version 3.0
- *
- * Ejercicio: Muebles de los Alpes
- * Autor: Juan Sebastián Urrego
- * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
- */
+
 package co.edu.uniandes.csw.mueblesdelosalpes.dto;
 
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * Clase que modela un vendedor dentro del sistema.
- * @author Juan Sebastián Urrego
- */
 public class Vendedor
 {
 
-    //-----------------------------------------------------------
-    // Atributos
-    //-----------------------------------------------------------
-
-    /**
-     * Número de identificación del vendedor
-     */
     private long id;
 
-    /**
-     * Nombres del vendedor.
-     */
     private String nombres;
 
-    /**
-     * Apellidos del vendedor.
-     */
     private String apellidos;
 
-    /**
-     * Lista de ítems de experiencia del vendedor.
-     */
     private List<ExperienciaVendedor> experiencia;
 
-    /**
-     * Salario del vendedor.
-     */
     private double salario;
 
-    /**
-     * Comisión por ventas para el vendedor.
-     */
     private double comisionVentas;
-
-    /**
-     * Perfil de vendedor.
-     */
+  
     private String perfil;
 
-    /**
-     * Foto del vendedor.
-     */
     private String foto;
 
-    //-----------------------------------------------------------
-    // Constructor
-    //-----------------------------------------------------------
-
-    /**
-     * Constructor sin argumentos
-     */
     public Vendedor()
     {
         experiencia = new ArrayList<ExperienciaVendedor>();
     }
 
-    /**
-     * Constructor con argumentos de la clase
-     * @param id Identificador único del vendedor
-     * @param nombres Nombre(s) del vendedor
-     * @param apellidos Apellido(s) del vendedor
-     * @param experiencia Lista con la experiencia laboral del vendedor
-     * @param salario Salario del vendedor
-     * @param comisionVentas Valor en comisión por ventas
-     * @param perfil Perfil del vendedor
-     * @param foto Nombre de la foto del vendedor
-     */
     public Vendedor(long id, String nombres, String apellidos, List<ExperienciaVendedor> experiencia, double salario, double comisionVentas, String perfil, String foto)
     {
         this.id = id;
@@ -100,23 +40,12 @@ public class Vendedor
         this.foto = foto;
     }
 
-    //-----------------------------------------------------------
-    // Getters y setters
-    //-----------------------------------------------------------
-
-    /**
-     * Devuelve el número único de identificación del vendedor
-     * @return id Número de identificación
-     */
+ 
     public long getIdentificacion()
     {
         return id;
     }
 
-    /**
-     * Modifica el número de identificación del cliente
-     * @param id Nuevo número de identificación
-     */
     public void setIdentificacion(long id)
     {
         this.id = id;
